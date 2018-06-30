a85760c392c5908f3bd8aa45ea6031a99f698928
Nurit Levi <nuritlh@gmail.com>
2018-06-30 16:58:06 +0300

    design

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -16,7 +16,7 @@ function addMarker(loc) {
   var marker = new google.maps.Marker({
     position: loc,
     map: map,
-    title: 'Hello World!'
+    title: 'Here is your location'
   });
   return marker;
 }
